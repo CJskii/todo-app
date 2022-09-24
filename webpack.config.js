8edaec6a6f9dd9e94a7a987eeb01d988e1f3e831
CJskii <czareczarek3@gmail.com>
2022-09-25 00:10:19 +0100

    html plugin + dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    entry: {
-        index: './src/index.js',
-        home: './src/home.js',
-      },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+ module.exports = {
+  mode: 'development',
+   entry: './src/index.js',
+   devtool: 'inline-source-map',
+   plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+   output: {
+     filename: 'bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true,
+   },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+              {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
-    ],
-  },
-};
+      ],
+    },
+ };

c548b53768e28d734b3969a4d3d44b0cfdbae2d4
CJskii <czareczarek3@gmail.com>
2022-10-15 19:05:51 +0100

    fresh start

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import _ from 'lodash'
 import './style.css';
-///import xxx from './icon.png';
-//import printMe from './home.js';
-import Buttons from './buttons';
-import Todos from './todos';
+
 
 function component() {
     const element = document.createElement('div');
@@ -11,9 +8,6 @@ function component() {
     const header = document.createElement('h1')
     header.textContent = "Todo App"
     element.appendChild(header)
-    element.appendChild(Buttons())
-    element.appendChild(Todos())
-    
 
     return element;
   }

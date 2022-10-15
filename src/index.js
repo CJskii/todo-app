2d578b01836b4a16c2f2575bc2bbf5da21fde3d6
CJskii <czareczarek3@gmail.com>
2022-10-15 21:34:47 +0100

    Todo object methods

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import _ from 'lodash'
+import _, { property } from 'lodash'
 import './style.css';
 
 
@@ -14,3 +14,62 @@ function component() {
   
 
   document.body.appendChild(component());
+
+  const todo = {
+    tasks: [],
+    newTask: function(title, description, priority) {
+      let index = todo.tasks.length
+      //const arrayCheck = todo.tasks.filter((obj) => obj.index === index)
+      //console.log(index)
+      this.tasks.push({index, title, description, priority})
+      return //component(title,description,priority)
+  }, 
+    getTasks: function () {
+        this.tasks.forEach((key) => {
+            console.log(key)
+        })
+    },
+    render: function(title,description,priority) {
+        // call component to render
+    },
+    delete: function(index){
+      todo.tasks.forEach((key =>{
+        if (key.index == index){
+          todo.tasks.splice(index, 1)
+          console.log(todo.tasks)
+        }
+      }))
+    },
+    edit: function(){
+
+    }
+  }
+
+  todo.newTask('Rubbish', "Take bin out", "1")
+  todo.newTask('Shopping', "Go to ASDA ", "2")
+  todo.newTask('Shopping', "Go to ASDA ", "2")
+  todo.newTask('Shopping', "Go to ASDA ", "2")
+  todo.newTask('Shopping', "Go to ASDA ", "2")
+  todo.newTask('Shopping', "Go to ASDA ", "2")
+  todo.newTask('Shopping', "Go to ASDA ", "2")
+  todo.newTask('Shopping', "Go to ASDA ", "2")
+  todo.newTask('Shopping', "Go to ASDA ", "2")
+  //todo.getTasks()
+  todo.delete(4)
+  todo.delete(2)
+  todo.delete(5)
+  todo.newTask('Shopping', "Go to ASDA ", "2")
+  //todo.getTasks()
+
+
+  /*const obj = todo.tasks.filter((object) =>{
+        return object.index === index
+      })
+      delete this.obj
+      todo.getTasks()
+      return console.log(obj)
+
+
+      console.log(this.tasks)
+      this.tasks = todo.tasks.filter(i => i.index === index)
+      console.log(todo.tasks)*/
